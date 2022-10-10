a376def2c439a304e01d580316537fd6050bbe51
Henry Le Grys <henry@legrys.info>
2022-10-10 15:37:35 +0100

    Add basic custom save API

---
+++ BaboonAPI/api/CustomSaveData.fs
@@ -0,0 +1,39 @@
+﻿namespace BaboonAPI.Hooks.Saves
+
+open BepInEx
+
+type ICustomSaveData =
+    abstract Save: unit -> obj
+    abstract Load: obj -> unit
+
+type SaverCapability =
+    abstract Attach: string -> ICustomSaveData -> unit
+
+type private PluginSaverLoader(pluginGuid: string, attacher: SaverCapability -> unit) =
+    member _.Save (pluginData: Map<string, obj>) =
+        let mutable pluginData = pluginData
+
+        attacher { new SaverCapability with
+                     member _.Attach name sd =
+                         pluginData <- pluginData.Add ($"{pluginGuid}/{name}", sd.Save()) }
+
+        pluginData
+
+    member _.Load (pluginData: Map<string, obj>) =
+        attacher { new SaverCapability with
+                     member _.Attach name sd =
+                         pluginData[$"{pluginGuid}/{name}"] |> sd.Load }
+
+module CustomSaveRegistry =
+    let mutable private pluginSavers: PluginSaverLoader list = []
+
+    let Register (info: PluginInfo) (attach: SaverCapability -> unit) =
+        pluginSavers <- PluginSaverLoader (info.Metadata.GUID, attach) :: pluginSavers
+        ()
+
+    let internal SaveAll () =
+        pluginSavers |> Seq.fold (fun state saver -> saver.Save state) Map.empty
+
+    let internal LoadAll (pluginData: Map<string, obj>) =
+        for saver in pluginSavers do
+            saver.Load(pluginData)
---
+++ BaboonAPI/patch/SaverLoaderPatch.fs
@@ -0,0 +1,65 @@
+﻿namespace BaboonAPI.Patch
+
+open System
+open System.IO
+open BaboonAPI.Hooks.Saves
+open BepInEx.Logging
+open HarmonyLib
+open Newtonsoft.Json
+open UnityEngine
+
+module private CustomSaveController =
+    let CustomSaveVersion = 1
+
+    exception IncompatibleVersion of int * int
+
+    [<Serializable>]
+    type BaboonSaveData =
+        { Version: int
+          PluginData: Map<string, obj> }
+
+    let private serializer = JsonSerializer.Create()
+
+    let dataPath (index: int) = $"{Application.persistentDataPath}/baboonapi_save_{index}.json"
+
+    let SavePluginData (index: int) =
+        let data =
+            { Version = CustomSaveVersion
+              PluginData = CustomSaveRegistry.SaveAll() }
+
+        use fd = File.Open(dataPath index, FileMode.Create)
+        use stream = new StreamWriter(fd)
+        use writer = new JsonTextWriter(stream)
+
+        serializer.Serialize (writer, data)
+
+    let LoadPluginData (index: int) =
+        use fd = File.Open(dataPath index, FileMode.Open)
+        use stream = new StreamReader(fd)
+        use reader = new JsonTextReader(stream)
+
+        let data = serializer.Deserialize<BaboonSaveData> reader
+        if data.Version = CustomSaveVersion then
+            CustomSaveRegistry.LoadAll data.PluginData
+        else
+            raise (IncompatibleVersion (CustomSaveVersion, data.Version))
+
+[<HarmonyPatch(typeof<SaverLoader>)>]
+type SaverLoaderPatch() =
+    static let logger = Logger.CreateLogSource "BaboonAPI.SaverLoaderPatch"
+
+    [<HarmonyPostfix>]
+    [<HarmonyPatch("loadSavedGame")>]
+    static member LoadPostfix() =
+        try
+            CustomSaveController.LoadPluginData(SaverLoader.global_saveindex)
+        with
+        | :? FileNotFoundException ->
+            logger.LogInfo "No custom save file yet, continuing"
+        | CustomSaveController.IncompatibleVersion (expected, actual) ->
+            logger.LogError $"The plugin save data is version {actual}, but this BaboonAPI version expects {expected}."
+
+    [<HarmonyPostfix>]
+    [<HarmonyPatch("updateSavedGame")>]
+    static member SavePostfix() =
+        CustomSaveController.SavePluginData(SaverLoader.global_saveindex)
