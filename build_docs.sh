63729112e269b6c258922ab0195d947e2de058a4
Henry Le Grys <henry@legrys.info>
2022-10-17 15:53:10 +0100

    Build the project first before building docs

--- build_docs.sh
+++ build_docs.sh
@@ -3,4 +3,8 @@
 export DOTNET_ROOT=/opt/buildhome/.dotnet
 
 dotnet tool install -g fsdocs-tool
-fsdocs build
+# Build the project first so fsdocs can read the XML file
+dotnet build
+
+# Build docs
+fsdocs build --parameters root /
