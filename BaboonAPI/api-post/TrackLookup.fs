11ec9fd8c614589151570394bc5627882f34d4f7
Henry Le Grys <henry@legrys.info>
2023-02-19 23:16:52 +0000

    Add public track lookup API

---
+++ BaboonAPI/api-post/TrackLookup.fs
@@ -0,0 +1,8 @@
+﻿/// Public hooks to look up registered tracks
+module BaboonAPI.Hooks.Tracks.TrackLookup
+
+open BaboonAPI.Internal
+
+/// Look up a track by trackref
+let public lookup (trackref: string): TromboneTrack =
+    TrackAccessor.fetchTrack trackref
