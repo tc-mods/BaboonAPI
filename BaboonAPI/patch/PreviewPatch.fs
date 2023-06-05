7b90e7a197c888064975a502692fc58de715a7a9
Henry Le Grys <henry@legrys.info>
2023-06-05 21:18:19 +0100

    Clean up reflection in preview patch

--- BaboonAPI/patch/PreviewPatch.fs
+++ BaboonAPI/patch/PreviewPatch.fs
@@ -1,6 +1,5 @@
 ﻿namespace BaboonAPI.Patch
 
-open System.Reflection
 open BaboonAPI.Hooks.Tracks
 open BaboonAPI.Internal
 open BaboonAPI.Utility.Coroutines
@@ -12,16 +11,19 @@ open UnityEngine
 type PreviewPatch() =
     static let logger = Logger.CreateLogSource "BaboonAPI.PreviewPatch"
 
-    static let setClipAndFade (clip: TrackAudio) (player: LevelSelectClipPlayer) =
-        let pt = player.GetType()
-        pt.GetField("clip_volume", BindingFlags.NonPublic ||| BindingFlags.Instance).SetValue (player, clip.Volume)
+    static let clip_volume_f = AccessTools.Field(typeof<LevelSelectClipPlayer>, "clip_volume")
+    static let clip_player_f = AccessTools.Field(typeof<LevelSelectClipPlayer>, "clipPlayer")
+    static let start_fade_m = AccessTools.Method(typeof<LevelSelectClipPlayer>, "startCrossFade")
 
-        let clipPlayer: AudioSource = unbox (pt.GetField("clipPlayer", BindingFlags.NonPublic ||| BindingFlags.Instance).GetValue player)
+    static let setClipAndFade (clip: TrackAudio) (player: LevelSelectClipPlayer) =
+        clip_volume_f.SetValue (player, clip.Volume)
+
+        let clipPlayer: AudioSource = unbox (clip_player_f.GetValue player)
         clipPlayer.clip <- clip.Clip
         clipPlayer.volume <- 0f
         clipPlayer.Play()
 
-        pt.GetMethod("startCrossFade", BindingFlags.NonPublic ||| BindingFlags.Instance).Invoke (player, [| 1f |])
+        start_fade_m.Invoke (player, [| 1f |])
         |> ignore
 
     static let doClipNotFound (player: LevelSelectClipPlayer) =
