437263fc52c644d22cd283eaf8d783747c7bba8f
Henry Le Grys <henry@legrys.info>
2023-11-23 14:26:24 +0000

    Fix accidentally quadratic track sorting
    apparently I forgot I was working with linked lists here

--- BaboonAPI/internal/TrackAccessor.fs
+++ BaboonAPI/internal/TrackAccessor.fs
@@ -37,7 +37,7 @@ let private checkForDuplicates (tracks: seq<string * RegisteredTrack>): seq<stri
 
 type TrackLoader() =
     let mutable tracks = Map.empty
-    let mutable tracksByIndex = []
+    let mutable tracksByIndex = Array.empty
 
     let makeTrackLoader () =
         TrackRegistrationEvent.EVENT.invoker.OnRegisterTracks()
@@ -45,33 +45,28 @@ type TrackLoader() =
             |> Seq.map (fun (i, track) -> track.trackref, { track = track; trackIndex = i })
             |> checkForDuplicates
 
-    member _.LoadTracks() =
-        tracks <- makeTrackLoader() |> Map.ofSeq
+    let onTracksLoaded (loaded: seq<string * RegisteredTrack>) =
+        // Our track sequence is already sorted - trackIndex is set above ^
+        let sortedTracks = loaded |> Seq.toArray
+        tracks <- Map.ofArray sortedTracks
+        tracksByIndex <- sortedTracks |> Array.map snd
 
-        let unsorted = tracks.Values |> List.ofSeq
-        tracksByIndex <- unsorted |> List.permute (fun i -> unsorted[i].trackIndex)
-
-        let allTracks = tracks.Values |> Seq.map (fun rt -> rt.track) |> Seq.toList
+        let allTracks = tracksByIndex |> Seq.map (fun rt -> rt.track) |> Seq.toList
         TracksLoadedEvent.EVENT.invoker.OnTracksLoaded allTracks
 
+    member _.LoadTracks() =
+        makeTrackLoader() |> onTracksLoaded
+
     member _.LoadTracksAsync () =
-        let onComplete output =
-            tracks <- output
-            let unsorted = tracks.Values |> List.ofSeq
-            tracksByIndex <- unsorted |> List.permute (fun i -> unsorted[i].trackIndex)
-
-            let allTracks = tracks.Values |> Seq.map (fun rt -> rt.track) |> Seq.toList
-            TracksLoadedEvent.EVENT.invoker.OnTracksLoaded allTracks
-
         coroutine {
             let task = Async.StartAsTask (async {
-                return makeTrackLoader() |> Map.ofSeq
+                return makeTrackLoader()
             })
 
             yield WaitForTask(task)
 
             if task.IsCompletedSuccessfully then
-                onComplete task.Result
+                onTracksLoaded task.Result
             else if task.IsFaulted then
                 raise task.Exception
         }
@@ -97,7 +92,7 @@ let fetchTrackIndex (ref: string) = trackLoader.lookup(ref).trackIndex
 
 let trackCount () = trackLoader.Tracks.Count
 
-let allTracks () = trackLoader.TracksByIndex |> Seq.ofList
+let allTracks () = trackLoader.TracksByIndex |> Seq.ofArray
 
 let toTrackData (track: TromboneTrack) = makeTrackData track (fetchTrackIndex track.trackref)
 
