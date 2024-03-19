cdafe0ac752298e7bfdb4215b0367c8fc25b9323
Henry Le Grys <henry@legrys.info>
2024-03-19 22:03:46 +0000

    Install .NET 8 for docs builds

--- build_docs.sh
+++ build_docs.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+pushd /tmp
+wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
+chmod u+x /tmp/dotnet-install.sh
+/tmp/dotnet-install.sh --channel 8.0
+popd
+
 export DOTNET_ROOT=/opt/buildhome/.dotnet
 
-# Install fsdocs-tool for .NET 6
-dotnet tool install --global fsdocs-tool --version 16.1.1
+# Install fsdocs-tool for .NET 8
+dotnet tool install --global fsdocs-tool --version 20.0.0
 
 # Build the project first so fsdocs can read the XML file
 dotnet build
