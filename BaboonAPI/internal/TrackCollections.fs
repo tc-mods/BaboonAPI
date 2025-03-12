e82f629eff990e76946ddbefbb62f0d95d5009cd
Henry Le Grys <henry@legrys.info>
2025-03-12 23:46:22 +0000

    Add logging for collection icon load errors

--- BaboonAPI/internal/TrackCollections.fs
+++ BaboonAPI/internal/TrackCollections.fs
@@ -3,11 +3,14 @@ namespace BaboonAPI.Hooks.Tracks.Collections
 open BaboonAPI.Hooks.Tracks
 open BaboonAPI.Internal
 open BaboonAPI.Utility.Coroutines
+open BepInEx.Logging
 open UnityEngine
 
 /// <summary>A lazily-resolved track collection</summary>
 [<AbstractClass>]
 type public BaseTromboneCollection(id: string, name: string, description: string) =
+    static let logger = Logger.CreateLogSource "BaboonAPI.TrackCollections"
+
     /// Resolve this collection's list of tracks.
     abstract BuildTrackList: unit -> TromboneTrack seq
 
@@ -29,7 +32,9 @@ type public BaseTromboneCollection(id: string, name: string, description: string
                 let art =
                     match sprite with
                     | Ok sprite -> sprite
-                    | Error _ -> null
+                    | Error err ->
+                        logger.LogWarning $"Failed to load an icon for collection '{id}': {err}"
+                        null
 
                 let tracks =
                     (this :> TromboneCollection).tracks
