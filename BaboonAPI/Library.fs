3288e20d492b92361ca1d8b5b769958627f3c1f6
Henry Le Grys <henry@legrys.info>
2025-03-13 20:31:43 +0000

    v2.8.1

--- BaboonAPI/Library.fs
+++ BaboonAPI/Library.fs
@@ -8,7 +8,7 @@ open BaboonAPI.Utility
 open BepInEx
 open HarmonyLib
 
-[<BepInPlugin("ch.offbeatwit.baboonapi.plugin", "BaboonAPI", "2.8.0.0")>]
+[<BepInPlugin("ch.offbeatwit.baboonapi.plugin", "BaboonAPI", "2.8.1.0")>]
 type BaboonPlugin() =
     inherit BaseUnityPlugin()
 
